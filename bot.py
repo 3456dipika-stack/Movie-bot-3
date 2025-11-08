4400ccc7c88dce6ff429931d304eafa90036bbba
3456dipika-stack <3456dipika@gmail.com>
2025-11-08 21:22:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -649,6 +649,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     welcome_text = (
         f"👋 Hello {user_mention}, I am an advanced filter bot. 🤖\n\n"
         "I can help you find files in this chat with ease. 🔎 Just send me the name of the file you're looking for!\n\n"
+        "Admin Kaustav Ray have 11lakh+ files. \n\n"
         "Click the buttons below to learn more about how I work. 👇"
     )
 
